c9501e56cc810c21fd0d895f75b406d35a9b0031
Majid E. William <Maged.EWilliam@gmail.com>
2020-06-12 14:17:44 +0200

    fixes after review

--- README.md
+++ README.md
@@ -1,12 +1,25 @@
 # Project: Landing Page
 
-## Table of Contents
+## Outlines
 
-* [Into](#intro)
+* [Landing page behavior](#Landing page behavior)
+* [Interface and Architecture](#Interface and Architecture)
 
-## Intro
+## Landing page behavior
 
 This project demonstrate a dynamically updating navigational menu based on the amount of sections that is added to the page.
 
+## Interface and Architecture
 
+The project structured is as follow:
 
+```
+css
+- styles.css    
+index.html
+js
+- app.js
+README.md
+```
+
+In this project we are using modern ES6/ES2015, with all the code are cross-browser💯, also we made sure to provide comment when needed
--- js/app.js
+++ js/app.js
@@ -40,27 +40,14 @@ const menuLink = (link, label)=> `<li><a href="#${link}" class="menu__link">${la
  * Is Element Visible In Viewport
  * {@link: https://stackoverflow.com/questions/123999/how-can-i-tell-if-a-dom-element-is-visible-in-the-current-viewport}
  *
- * @param  {el}  html element
+ * @param  {section}  html element
  * @return {Boolean}
  */
-function isElementVisibleInViewport(el) {
-  var top = el.offsetTop;
-  var left = el.offsetLeft;
-  var width = el.offsetWidth;
-  var height = el.offsetHeight;
-
-  while(el.offsetParent) {
-    el = el.offsetParent;
-    top += el.offsetTop;
-    left += el.offsetLeft;
-  }
-
-  return (
-    top >= window.pageYOffset &&
-    left >= window.pageXOffset &&
-    (top + height) <= (window.pageYOffset + window.innerHeight) &&
-    (left + width) <= (window.pageXOffset + window.innerWidth)
-  );
+function isElementVisibleInViewport(section) {
+	if(section.getBoundingClientRect().top < window.innerHeight && section.getBoundingClientRect().top > 0){
+		return true;
+	}
+	return false;
 }
 
 /**
